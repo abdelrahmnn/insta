7ae67576a658199d05e257d1bf6fd347dbf48d61
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-04-13 16:26:09 +0200

    Linking pages icon Navigation Bar

--- lib/responive/mobile.dart
+++ lib/responive/mobile.dart
@@ -1,7 +1,13 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:instagram/screens/add_post.dart';
+import 'package:instagram/screens/home.dart';
+import 'package:instagram/screens/profile.dart';
+import 'package:instagram/screens/search.dart';
 import 'package:instagram/shared/colors.dart';
 
 class mobileSerren extends StatefulWidget {
@@ -12,47 +18,63 @@ class mobileSerren extends StatefulWidget {
 }
 
 class _mobileSerrenState extends State<mobileSerren> {
+  final PageController _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("mobile Serren"),
-      ),
-      bottomNavigationBar: CupertinoTabBar(
-          backgroundColor: mobileBackgroundColor,
-          onTap: (index) {},
-          items: [
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.home,
-                  color: primaryColor,
-                ),
-                label: "home"),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.search,
-                  color: secondaryColor,
-                ),
-                label: "home"),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.add_circle,
-                  color: secondaryColor,
-                ),
-                label: "home"),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.favorite,
-                  color: secondaryColor,
-                ),
-                label: "home"),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.person,
-                  color: secondaryColor,
-                ),
-                label: "home"),
-          ]),
-    );
+        appBar: AppBar(
+          title: Text("mobile Serren"),
+        ),
+        bottomNavigationBar: CupertinoTabBar(
+            backgroundColor: mobileBackgroundColor,
+            onTap: (index) {
+              // navigate to the tabed page
+              _pageController.jumpToPage(index);
+            },
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home,
+                    color: primaryColor,
+                  ),
+                  label: "home"),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.search,
+                    color: secondaryColor,
+                  ),
+                  label: "home"),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.add_circle,
+                    color: secondaryColor,
+                  ),
+                  label: "home"),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.favorite,
+                    color: secondaryColor,
+                  ),
+                  label: "home"),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.person,
+                    color: secondaryColor,
+                  ),
+                  label: "home"),
+            ]),
+        body: PageView(
+          //      onPageChanged: (index) {},
+          physics: NeverScrollableScrollPhysics(),
+          controller: _pageController,
+          children: [
+            home(),
+            search(),
+            profile(),
+            Center(child: Text("chid")),
+            add_post(),
+          ],
+        ));
   }
 }
