cd5f65b5787dc72cf8ab4562fe31eb961374b702
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-04-13 15:28:23 +0200

    Change the color of the Navigation Bar again through a color file

--- lib/responive/mobile.dart
+++ lib/responive/mobile.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:instagram/shared/colors.dart';
 
 class mobileSerren extends StatefulWidget {
   const mobileSerren({super.key});
@@ -17,7 +18,8 @@ class _mobileSerrenState extends State<mobileSerren> {
       appBar: AppBar(
         title: Text("mobile Serren"),
       ),
-      bottomNavigationBar: CupertinoTabBar(items: [
+      bottomNavigationBar:
+          CupertinoTabBar(backgroundColor: mobileBackgroundColor, items: [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
         BottomNavigationBarItem(icon: Icon(Icons.search), label: "home"),
       ]),
