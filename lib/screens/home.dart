b60818a9fefb039b9bd5eb36dce725fc7ac18e1d
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-04-24 05:41:11 +0200

    Change background color web

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -19,7 +19,8 @@ class _homeState extends State<home> {
   Widget build(BuildContext context) {
     final double widthScreen = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: mobileBackgroundColor,
+      backgroundColor:
+          widthScreen > 600 ? webBackgroundColor : mobileBackgroundColor,
       appBar: widthScreen > 600
           ? null
           : AppBar(
@@ -42,136 +43,140 @@ class _homeState extends State<home> {
                 height: 32,
               ),
             ),
-      body: Column(children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 13),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Container(
-                    padding: EdgeInsets.all(3),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color.fromARGB(125, 78, 91, 110),
+      body: Container(
+        color: mobileBackgroundColor,
+        margin: EdgeInsets.symmetric(vertical: 55, horizontal: 100),
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 13),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(3),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color.fromARGB(125, 78, 91, 110),
+                      ),
+                      child: CircleAvatar(
+                        radius: 26,
+                        backgroundImage: NetworkImage(
+                            "https://scontent.fcai21-4.fna.fbcdn.net/v/t39.30808-6/248379584_3759270507631363_3366834168759626723_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeE5ci7MNra4sLn0KSchARj8Q4bWcuEvP4hDhtZy4S8_iHXKc4R0JBSp0rIxEQp9VhgeWPRSy4Ustgy_hiyPEU8I&_nc_ohc=n5gOpNI7GZsAX_7L_cT&_nc_ht=scontent.fcai21-4.fna&oh=00_AfCywPN8Kn2KRmSkRF-dJ479Uo51A6sbJjjVoEu9YBSWUg&oe=644A50EF"),
+                      ),
                     ),
-                    child: CircleAvatar(
-                      radius: 26,
-                      backgroundImage: NetworkImage(
-                          "https://scontent.fcai21-4.fna.fbcdn.net/v/t39.30808-6/248379584_3759270507631363_3366834168759626723_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeE5ci7MNra4sLn0KSchARj8Q4bWcuEvP4hDhtZy4S8_iHXKc4R0JBSp0rIxEQp9VhgeWPRSy4Ustgy_hiyPEU8I&_nc_ohc=n5gOpNI7GZsAX_7L_cT&_nc_ht=scontent.fcai21-4.fna&oh=00_AfCywPN8Kn2KRmSkRF-dJ479Uo51A6sbJjjVoEu9YBSWUg&oe=644A50EF"),
+                    SizedBox(
+                      width: 17,
                     ),
-                  ),
-                  SizedBox(
-                    width: 17,
-                  ),
-                  Text(
-                    "Abdelrhman",
-                    style: TextStyle(fontSize: 15),
-                  ),
-                ],
-              ),
-              IconButton(
+                    Text(
+                      "Abdelrhman",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ],
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.more_vert,
+                    ))
+              ],
+            ),
+          ),
+          Image.network(
+            "https://images.pexels.com/photos/699122/pexels-photo-699122.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            fit: BoxFit.cover,
+            height: MediaQuery.of(context).size.height * 0.35,
+            width: double.infinity,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 11),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.favorite_border),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.comment_outlined,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.send,
+                      ),
+                    ),
+                  ],
+                ),
+                IconButton(
                   onPressed: () {},
                   icon: Icon(
-                    Icons.more_vert,
-                  ))
-            ],
-          ),
-        ),
-        Image.network(
-          "https://images.pexels.com/photos/699122/pexels-photo-699122.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-          fit: BoxFit.cover,
-          height: MediaQuery.of(context).size.height * 0.35,
-          width: double.infinity,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 11),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.favorite_border),
+                    Icons.bookmark_outline,
                   ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.comment_outlined,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.send,
-                    ),
-                  ),
-                ],
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.bookmark_outline,
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
-        ),
-        Container(
-          margin: EdgeInsets.fromLTRB(10, 0, 0, 10),
-          width: double.infinity,
-          child: Text(
-            "10 likes",
-            textAlign: TextAlign.start,
-            style:
-                TextStyle(fontSize: 18, color: Color.fromARGB(255, 78, 81, 94)),
-          ),
-        ),
-        Row(
-          children: [
-            SizedBox(
-              width: 9,
-            ),
-            Text(
-              "Abdelrhman",
-              textAlign: TextAlign.start,
-              style: TextStyle(
-                  fontSize: 20, color: Color.fromARGB(255, 189, 196, 199)),
-            ),
-            Text(
-              "  Hello ;-)",
-              textAlign: TextAlign.start,
-              style: TextStyle(
-                  fontSize: 15, color: Color.fromARGB(255, 189, 196, 199)),
-            ),
-          ],
-        ),
-        GestureDetector(
-          onTap: () {},
-          child: Container(
-            margin: EdgeInsets.fromLTRB(10, 9, 9, 10),
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 0, 0, 10),
             width: double.infinity,
             child: Text(
-              "100 cooments",
+              "10 likes",
+              textAlign: TextAlign.start,
               style: TextStyle(
-                  fontSize: 16, color: Color.fromARGB(255, 78, 81, 94)),
+                  fontSize: 18, color: Color.fromARGB(255, 78, 81, 94)),
+            ),
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 9,
+              ),
+              Text(
+                "Abdelrhman",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                    fontSize: 20, color: Color.fromARGB(255, 189, 196, 199)),
+              ),
+              Text(
+                "  Hello ;-)",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                    fontSize: 15, color: Color.fromARGB(255, 189, 196, 199)),
+              ),
+            ],
+          ),
+          GestureDetector(
+            onTap: () {},
+            child: Container(
+              margin: EdgeInsets.fromLTRB(10, 9, 9, 10),
+              width: double.infinity,
+              child: Text(
+                "100 cooments",
+                style: TextStyle(
+                    fontSize: 16, color: Color.fromARGB(255, 78, 81, 94)),
+                textAlign: TextAlign.start,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 0, 0, 10),
+            width: double.infinity,
+            child: Text(
+              "10 june 2022",
+              style: TextStyle(
+                  fontSize: 15, color: Color.fromARGB(255, 78, 81, 94)),
               textAlign: TextAlign.start,
             ),
           ),
-        ),
-        Container(
-          margin: EdgeInsets.fromLTRB(10, 0, 0, 10),
-          width: double.infinity,
-          child: Text(
-            "10 june 2022",
-            style:
-                TextStyle(fontSize: 15, color: Color.fromARGB(255, 78, 81, 94)),
-            textAlign: TextAlign.start,
-          ),
-        ),
-      ]),
+        ]),
+      ),
     );
   }
 }
