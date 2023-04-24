5e481763621d9abf6d0334974d9a15afc93c2d34
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-04-24 05:19:51 +0200

    add_a_photo icon

--- lib/responive/web.dart
+++ lib/responive/web.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:instagram/shared/colors.dart';
 
 class webSerren extends StatefulWidget {
@@ -30,7 +31,34 @@ class _webSerrenState extends State<webSerren> {
             ),
             onPressed: () {},
           ),
+          IconButton(
+            icon: Icon(
+              Icons.add_a_photo,
+              color: secondaryColor,
+            ),
+            onPressed: () {},
+          ),
+          //   IconButton(
+          //     icon: Icon(
+          //       Icons.favorite,
+          //       color: secondaryColor,
+          //     ),
+          //     onPressed: () {},
+          //   ),
+          //   IconButton(
+          //     icon: Icon(
+          //       Icons.person,
+          //       color: secondaryColor,
+          //     ),
+          //     onPressed: () {},
+          //   ),
         ],
+        backgroundColor: mobileBackgroundColor,
+        title: SvgPicture.asset(
+          "assets/img/instagram.svg",
+          color: primaryColor,
+          height: 32,
+        ),
       ),
     );
   }
