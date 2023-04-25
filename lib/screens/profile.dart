9be4b36e511fb9005b3ba9504e0f3e26902add7e
abdorady2022001 <119637147+abdorady2022001@users.noreply.github.com>
2023-04-25 03:11:24 +0200

    Image download page

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:instagram/shared/colors.dart';
 
 class profile extends StatefulWidget {
   const profile({super.key});
@@ -13,8 +14,14 @@ class _profileState extends State<profile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("home profile"),
+      backgroundColor: mobileBackgroundColor,
+      body: Center(
+        child: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.upload,
+              size: 55,
+            )),
       ),
     );
   }
